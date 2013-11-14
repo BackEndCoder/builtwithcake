6ecfcabd23de0a4f80219378d55674616901e48b
David Yell <dyell@ukwebmedia.com>
2013-11-14 17:21:19 +0000

    Started on the Projects stack

---
+++ app/Controller/ProjectsController.php
@@ -0,0 +1,12 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Description of ProjectsController
+ *
+ * @author David Yell <neon1024@gmail.com>
+ */
+class ProjectsController extends AppController {
+
+	
+
+}
